6f473b8719f394ee7a74ffededa8e0f4a570353d
jisoo123456 <imearth0913@gmail.com>
2023-02-04 16:21:02 +0900

    task_0203-2

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,19 +1,19 @@
 def find_and_insert_data(pokemon, health):
 	find_pos = -1
 	list_pokemons_keys = list(pokemons.keys())
-	list_pokemons_value = list(pokemons.values())
+	list_pokemons_value = list(pokemons.values())  #[100, 80, 30, 10]
 
-	for i in range(len(list_pokemons_value)):
-		pair = list_pokemons_value[i]
-		if health >= pair:
-			find_pos = i
+	for i in range(len(list_pokemons_value)):  #4
+		pair = list_pokemons_value[i]  #0~3
+		if health >= pair:  #50 > 30
+			find_pos = i  #2
 			break
 	if find_pos == -1:
 		find_pos = len(pokemons)
 
 	insert_data(find_pos, pokemon, health)
 
-def insert_data(position, pokemon, health):
+def insert_data(position, pokemon, health):  #2
 	if position < 0 or position > len(pokemons):
 		print("데이터를 삽입할 범위를 벗어났습니다.")
 		return
@@ -25,10 +25,10 @@ def insert_data(position, pokemon, health):
 	list_pokemons_keys = list(pokemons.keys())
 	list_pokemons_values = list(pokemons.values())
 
-	for i in range(position):
-		first_pokemons.update(list_pokemons_keys[i])
-	for k in range(position, len(pokemons)):
-		last_pokemons.update(list_pokemons_keys[k])
+	for i in range(position):  #0~1
+		first_pokemons.update({list_pokemons_keys[i] : list_pokemons_values[i]})  #피카츄 파이리
+	for k in range(position, len(pokemons)):  #2~3
+		last_pokemons.update({list_pokemons_keys[k] : list_pokemons_values[k]})  #꼬부기 라이츄
 
 	new_pokemons = {}
 	if position == 0:
