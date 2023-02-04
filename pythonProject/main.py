a261e835fe97d9c05b2992d3788603804177fa1a
jisoo123456 <imearth0913@gmail.com>
2023-02-04 14:21:08 +0900

    task_0203-1

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,37 +1,53 @@
-#시간이 부족해서 다 못했습니다. 시간이 마무리 되서 일단 오늘 안에 완성해서 제 깃에 업로드하겠습니다
 def find_and_insert_data(pokemon, health):
 	find_pos = -1
-	for i in range(len(pokemons)) : #(0~3)
-		pair = pokemons.values(i)
-		if health >= pair :  #i = 2
-			find_pos = i     #find_pos = 2
+	list_pokemons_keys = list(pokemons.keys())
+	list_pokemons_value = list(pokemons.values())
+
+	for i in range(len(list_pokemons_value)):
+		pair = list_pokemons_value[i]
+		if health >= pair:
+			find_pos = i
 			break
 	if find_pos == -1:
-		findPos = len(pokemons)
+		find_pos = len(pokemons)
 
-	insert_data(find_pos, (pokemon, health))
+	insert_data(find_pos, pokemon, health)
 
-
-def insert_data(position, **pokemon):
-	if position < 0 or position > len(pokemon):
+def insert_data(position, pokemon, health):
+	if position < 0 or position > len(pokemons):
 		print("데이터를 삽입할 범위를 벗어났습니다.")
 		return
 
-	pokemons.append(None)
+	append_pokemons = {pokemon : health}
+	first_pokemons = {}
+	last_pokemons = {}
 
-	for i in range(len(pokemons) - 1, position, -1):
-		pokemons[i] = pokemons[i - 1]
+	list_pokemons_keys = list(pokemons.keys())
+	list_pokemons_values = list(pokemons.values())
 
-	pokemons[position] = pokemon
+	for i in range(position):
+		first_pokemons.update(list_pokemons_keys[i])
+	for k in range(position, len(pokemons)):
+		last_pokemons.update(list_pokemons_keys[k])
 
+	new_pokemons = {}
+	if position == 0:
+		new_pokemons.update(append_pokemons)
+		new_pokemons.update(first_pokemons)
+		new_pokemons.update(last_pokemons)
 
-pokemons = {"피카츄" : 100, "파이리" : 80, "꼬부기" : 30, "라이츄" : 10}  #이상해 50[피카츄 파이리 이상해 꼬부기 라이츄]
+	else:
+		new_pokemons.update(first_pokemons)
+		new_pokemons.update(append_pokemons)
+		new_pokemons.update(last_pokemons)
 
+	print(new_pokemons)
 
-if __name__ == "__main__":
+pokemons = {"피카츄" : 100, "파이리" : 80, "꼬부기" : 30, "라이츄" : 10}
+
+if __name__=="__main__":
 
 	while True:
-		pokemon = input("추가할 포켓몬--> ")
-		health = int(input(f"{pokemon}의 능력치--> "))
+		pokemon = input("추가할 포켓몬:")
+		health = int(input(f'{pokemon}의 능력치:'))
 		find_and_insert_data(pokemon, health)
-		print(pokemons)
