207660697c82e9b72037bc5460ec9f8d310d7acc
jisoo123456 <imearth0913@gmail.com>
2023-02-06 21:13:29 +0900

    Task_2(0206)

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,112 +1,56 @@
-class Node():
-    def __int__(self):
-        self.data = None
-        self.node = None
+import random
 
-def print_nodes(start):
-    current = start
-    print(current.data, end = ' ')
-    while current.link != start:
-        pre = current  #
-        current = pre.link  #
-        print(current.data, end = ' ')
-    print()
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE - 1:
+        return True
+    else:
+        return False
 
+def is_stack_empty():
+    global SIZE, stack, top
+    if top == -1:
+        return True
+    else:
+        return False
 
-memory = []
-head, current, pre = None, None, None
-data_array = ['다현', '정연', '쯔위', '사나', '지효']
-
-def insert_node(find_data, insert_data):
-    global memory, head, current, pre
-
-    if find_data == head.data:
-        node = Node()
-        node.data = insert_data
-        node.link = head
-        last = head
-        while last.link != head:
-            last = last.link
-        last.link = node
-        head = node
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
         return
+    else:
+        top = top + 1
+        stack[top] = data
 
-    current = head
-    while current.link != head:
-        pre = current
-        current = current.link
-        if current.data == find_data:
-            node = Node()
-            node.data = insert_data
-            node.link = current
-            pre.link = node
-            return
+def pop():
+    global SIZE, stack, top
 
-    node = Node()
-    node.data = insert_data
-    current.link = node
-    node.link = head
-
-def delete_node(delete_node):
-    global memory, head, current, pre
-
-    if head.data == delete_node:
-        current = head
-        head = head.link
-        last = head
-        while last.link != current:
-            last = last.link
-        last.link = head
-        del(current)
+    if is_stack_empty():
         return
+    else:
+        data = stack[top]
+        stack[top] = None
+        top = top - 1
+        return data
 
-    current = head
-    while current.link != head:
-        pre = current
-        current = current.link
-        if current.data == delete_node:
-            pre.link = current.link
-            del(current)
-            return
-
-def find_node(find_data):
-    global memory, head, current, pre
-
-    current = head
-    if current.data == find_data:
-        return current
-    while current.link != head:
-        current = current.link
-        if current.data == find_data:
-            return current
-    return Node()  #여기서 node의미랑 none출력하고 싶으면?
+SIZE = 6
+stack = [None for _ in range(SIZE)]
+top = -1
 
 if __name__=="__main__":
-    node = Node()
-    node.data = data_array[0]
-    head = node
-    node.link = node
-    memory.append(node)
+    stone_color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    random.shuffle(stone_color)
 
-    for data in data_array[1:]:
-        pre = node
-        node = Node()
-        node.data = data
-        pre.link = node
-        node.link = head
-        memory.append(node)
+    print("go snack house:", end = ' ')
+    for stone in stone_color:
+        push(stone)
+        print(stone, '->', end = ' ')
+    print("snack house")
 
-    print_nodes(head)
-
-    fNode = find_node("다현")
-    print(fNode.data)
-
-    fNode = find_node("쯔위")
-    print(fNode.data)
-
-    print(find_node("쯔위").data)
-
-    fNode = find_node("재남")
-    print(fNode.data)
-    print(find_node("재남").data)
-
+    print("go home:", end = ' ')
+    while True:
+        stone = pop()
+        if stone == None:
+            break
+        print(stone, '->', end = ' ')
+    print("home")
