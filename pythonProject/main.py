2a9e9e6fe4906ad63715b510b3b959dc655999db
jisoo123456 <imearth0913@gmail.com>
2023-02-07 20:39:14 +0900

    task(chapter8 -1)

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,61 +1,47 @@
+import random
+
 class tree_node:
     def __init__(self):
-        self.left = None
         self.data = None
+        self.left = None
         self.right = None
 
-node1 = tree_node()
-node1.data = 'hasa'
+data_array = ['바나나맛우유', '레쓰비캔커피', '츄파츕스', '도시락', '삼다수', '코카콜라', '삼각김밥']
+sale_array = [random.choice(data_array) for _ in range(20)]
 
-node2 = tree_node()
-node2.data = 'sola'
-node1.left = node2
 
-node3 = tree_node()
-node3.data = 'monbyeal'
-node1.right = node3
+node = tree_node()
+node.data = sale_array[0]
+root = node
 
-node4 = tree_node()
-node4.data = 'hyheen'
-node2.left = node4
+for sale in sale_array[1:]:
+    node = tree_node()
+    node.data = sale
 
-node5 = tree_node()
-node5.data = 'zyee'
-node2.right = node5
-
-node6 = tree_node()
-node6.data = 'sunmi'
-node3.left = node6
+    current = root
+    while True:
+        if sale < current.data:
+            if current.left == None:
+                current.left = node
+                break
+            else:
+                current = current.left
+        elif sale > current.data:
+            if current.right == None:
+                current.right = node
+                break
+            else:
+                current = current.right
+        else:
+            del(node)
+            break
 
 def preorder(node):
     if node == None:
         return
-    print(node.data, end = '->')  #화사
-    preorder(node.left) #preorder(sola) -> sola -> preorder(hyheen) -> hyheen -> out preorder(hyheen) -> preorder(zyeel)
-                        #zyee -> out preorder(zyee) -> out preorder(sola) -> preorder(monbyeal) -> monbyeal
-                        # preorder(sunmi) -> sunmi -> out preorder(sunmi) -> preorder(monbyeal.right) -> return
+    print(node.data, end = ' ')
+    preorder(node.left)
     preorder(node.right)
 
-
-def inorder(node):
-    if node == None:
-        return
-    inorder(node.left)
-    print(node.data, end='->')
-    inorder(node.right)
-
-def postorder(node):
-    if node == None:
-        return
-    postorder(node.left)
-    postorder(node.right)
-    print(node.data, end='->')
-
-print("preorder: ", end = ' ')
-preorder(node1)
-print()
-print("inorder: ", end = ' ')
-inorder(node1)
-print()
-print("postorder: ", end = ' ')
-postorder(node1)
+print(sale_array)
+preorder(root)
