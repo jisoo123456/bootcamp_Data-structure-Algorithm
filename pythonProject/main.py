0c8167fc90ba7073f96bd28bbd93a77f9b866d69
jisoo123456 <imearth0913@gmail.com>
2023-02-08 13:43:45 +0900

    BFS(deque_module)

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,26 +1,26 @@
 #너비우선탐색(큐사용)
-
+from collections import deque
 class Graph:
 	def __init__ (self, size) :
 		self.SIZE = size
 		self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
 
-G1 = None
-queue = []
+g = None
+queue = deque([])
 visited_array = []
 
 
-G1 = Graph(4)
-G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
+g = Graph(4)
+g.graph[0][2] = 1; g.graph[0][3] = 1
+g.graph[1][2] = 1
+g.graph[2][0] = 1; g.graph[2][1] = 1; g.graph[2][3] = 1
+g.graph[3][0] = 1; g.graph[3][2] = 1
 
 print('## G1 무방향 그래프 ##')
 for row in range(4) :
 	for col in range(4) :
-		print(G1.graph[row][col], end = ' ')
+		print(g.graph[row][col], end =' ')
 	print()
 
 current = 0
@@ -30,7 +30,7 @@ visited_array.append(current)
 while len(queue) != 0:
 	next = None
 	for vertex in range(4) :
-		if G1.graph[current][vertex] == 1 :
+		if g.graph[current][vertex] == 1 :
 			if vertex in visited_array :
 				pass
 			else :
@@ -42,7 +42,7 @@ while len(queue) != 0:
 		queue.append(current)
 		visited_array.append(current)
 	else :
-		current = queue.pop(0)  #overhead
+		current = queue.popleft()
 
 
 for i in visited_array :
