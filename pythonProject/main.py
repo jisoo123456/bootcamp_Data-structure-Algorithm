6423780ea960a50c5e3b0fdc7b874389266a7ab3
jisoo123456 <imearth0913@gmail.com>
2023-02-14 16:05:01 +0900

    test

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,16 +1,1 @@
-import socket
-
-print("에코 클라이언트 시작됨")
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Stream은 TCP, datagram은 UDP
-client_socket.connect(("165.246.115.165", 20000))
-print("서버에 연결됨")
-
-while True:
-    msg = input("전송 메세지 입력: ")
-    if msg == "exit" or "EXIT":  #종료 메세지
-        break
-    client_socket.sendall((msg+"\n").encode())  #송신
-    data = client_socket.recv(1024).decode()  #수신 (1바이트로)
-    print(f"서버로부터 받은 메세지: {data}")
-
-client_socket.close()
+#test
